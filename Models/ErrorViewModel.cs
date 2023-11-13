a760dd71151978c56e844995610e8efa7d8e41d9
Kaique Santos <kaiquep00@hotmail.com>
2023-11-13 17:44:51 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WorldSound.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
